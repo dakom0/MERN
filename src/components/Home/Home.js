c4c1666710e29d29af42066f12d4bc15b0b1f67c
DAMI KOMOLAFE <damik94@gmail.com>
2020-05-31 19:27:26 -0400

    updated with images

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,6 +6,10 @@ class Home extends Component {
     return (
       <div>
         <h1>This is the homepage!</h1>
+       <a href="https://giphy.com/gifs/fAEGN7WzyfoRrLZaMH"> 
+       <iframe title="gif" src="https://giphy.com/embed/fAEGN7WzyfoRrLZaMH" 
+       width="480" height="300" frameBorder="0" class="giphy-embed" allowFullScreen>
+        </iframe></a>    
         <p>Where you can Read, Update, Delete or even Create your objects from the API</p>
         <p>This is the API where you can observe the changes</p>
         <a href="https://dakom1-crud-api.herokuapp.com/lists">Click Here</a>
